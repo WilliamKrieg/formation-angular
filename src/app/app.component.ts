cb4cd9ccf9bb7d3b984071d478fea4dabcd7317b
William <william.krieg@web-atrio.com>
2019-09-20 16:07:13 +0200

    Notre premier composant
    Bouton d'action et démarrage d'un timer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'FormationAngular';
+  public formation = false;
+  public timeRemaining = 0;
+  public timeInterval;
+
+  public startAndStopFormation(statut: boolean = false) {
+    this.formation = statut;
+    if (this.formation) {
+      this.timeRemaining = 20;
+      this.timeInterval = setInterval(() => {
+        this.timeRemaining--;
+        if (this.timeRemaining === 0) {
+          this.formation = false;
+          window.clearInterval(this.timeInterval);
+        }
+      }, 1000);
+    }
+  }
 }
