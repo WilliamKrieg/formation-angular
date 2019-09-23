423eac6716600c0852da9d9e6af3d597dcc9fc9c
William <william.krieg@web-atrio.com>
2019-09-23 16:50:38 +0200

    Fermer une souscription de données

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Post } from './models/post';
 import { PostsService } from './services/posts.service';
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,17 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
   providers: [PostsService]
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
 
   public posts: Post[];
   public postSelected: Post;
 
+  public subPost: Subscription;
+
   constructor(private postService: PostsService) {
-    this.postService.getPosts().subscribe(res => this.posts = res);
+    this.subPost = this.postService.getPosts().subscribe(res => this.posts = res);
   }
 
-  public testIfPostIsSelected(post: Post) : boolean{
+  public testIfPostIsSelected(post: Post): boolean {
     return (this.postSelected && post && this.postSelected.id === post.id);
+  }
+
+  ngOnDestroy(){
+    this.subPost.unsubscribe();
   }
 
 }
